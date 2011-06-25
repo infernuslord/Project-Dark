5e95629f0158ab3cb23c22eed954c699203716cf
Eli K <eliplan312@gmail.com>
2011-06-25 12:47:36 -0700

    Hopefully re-added all the source

---
+++ run_studiomdl.bat
@@ -0,0 +1,2 @@
+@"c:\program files (x86)\steam\steamapps\eliplan312\sourcesdk\bin\source2007\bin\studiomdl.exe" -game "c:\program files (x86)\steam\steamapps\SourceMods\projectdark" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
